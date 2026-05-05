254f1d1af1fe6cf9b14fcb7400499539dfc045c8
Lena(no) <mparracesar85@gmail.com>
2026-05-05 14:20:25 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -79,8 +79,10 @@ for (let i = 0; i < starsCount; i++) {
     star.style.width = size + "px";
     star.style.height = size + "px";
 
-    star.style.left = Math.random() * window.innerWidth + "px";
-    star.style.top = Math.random() * window.innerHeight + "px";
+    star.style.left = Math.random() * 100 + "vw";
+    star.style.top = Math.random() * 100 + "vh";
+    //star.style.left = Math.random() * window.innerWidth + "px";
+    //star.style.top = Math.random() * window.innerHeight + "px";
 
     star.style.animationDuration = (Math.random() * 5 + 5) + "s";
 
